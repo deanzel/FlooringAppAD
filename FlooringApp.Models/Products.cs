4cff8f0284d258419772e79712e9add3b814378a
Andrew Tang <atang.business@gmail.com>
2015-10-13 09:04:43 -0400

    added taxes and products abstract class

---
+++ FlooringApp.Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringApp.Models
+{
+    public abstract class Products
+    {
+        private string ProductType;
+        private decimal CostPerSquareFoot;
+        private decimal LaborCostPerSquareFoot;
+
+        private decimal MaterialsCostCalculator(decimal area);
+    }
+}
---
+++ FlooringApp.Models/Tax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringApp.Models
+{
+    public abstract class Tax
+    {
+        private string StateAbbreviation;
+        private string StateName;
+        private decimal TaxRate;
+
+        private decimal TaxCalculator(decimal subtotal);
+    }
+}
