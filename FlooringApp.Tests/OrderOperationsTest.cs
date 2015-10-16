5f841511af2e2bc19dfb386f684c2827dd36c193
Andrew Tang <atang.business@gmail.com>
2015-10-16 09:45:15 -0400

    started unit teseting

---
+++ FlooringApp.Tests/OrderOperationsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlooringApp.BLL;
+using FlooringApp.Models;
+using NUnit.Framework;
+
+namespace FlooringApp.Tests
+{
+    [TestFixture]
+    public class OrderOperationsTest
+    {
+        OrderOperations oops;
+
+        [SetUp]
+        public void oopSetup()
+        {
+            oops = new OrderOperations();
+        }
+
+        [TestCase(DateTime.Now, {})]
+        public void GetOrdersFromDateTest(DateTime input, Response expected)
+        {
+            Response actual = oops.GetOrdersFromDate(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
